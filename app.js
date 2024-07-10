9fda5d1887edf472ceadd49adf68772fee0470e4
nyallGadhiya <nyalgadhiya335@gmail.com>
2024-07-10 18:35:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const socketIO = require('socket.io')
+const express= require('express')
+const app = express()
+const http = require('http')
+const serverless = require("serverless-http");
+const { Socket } = require('dgram')
+
+app.set('view engine','ejs')
+
+
+const server = http.createServer(app)
+const io = socketIO(server)
+
+
+io.on("connection",(socket)=>{
+    socket.on("send-location",(data)=>{
+    io.emit("receive-location",{id:socket.id, ...data })
+    });
+    console.log("connected");
+
+    socket.on("disconnect",()=>{
+        io.emit("user-disconnect",socket.id)
+    })
+})
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+
+server.listen(3000)
+
+module.exports.handler = serverless(app);
---
+++ public/js/script.js
@@ -0,0 +1,20 @@
+const socket = io();
+
+if(navigator.geolocation)
+{
+    navigator.geolocation.watchPosition((position)=>{
+        const {latitude,longitude}=position.coords;
+        socket.emit("send-location",(latitude,longitude))
+    },
+    (error)=>{
+        console.error(error);
+    },
+    {
+        enableHighAccuracy:true,
+        maximumAge:0,
+        timeout:5000
+    }
+)
+}
+
+L.map("map");
