c851ba0f42e63646fd19ecbe24163401effa4db0
nyallGadhiya <nyalgadhiya335@gmail.com>
2024-07-10 19:14:02 +0530

    s

--- app.js
+++ app.js
@@ -1,33 +1,31 @@
-const socketIO = require('socket.io')
-const express= require('express')
-const app = express()
-const http = require('http')
-var path = require('path');
-const { Socket } = require('dgram')
+const socketIO = require('socket.io');
+const express = require('express');
+const http = require('http');
+const path = require('path');
 
-app.set('view engine','ejs');
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-
-const server = http.createServer(app)
-const io = socketIO(server)
-
-
-io.on("connection",(socket)=>{
-    socket.on("send-location",(data)=>{
-    io.emit("receive-location",{id:socket.id, ...data })
+io.on("connection", (socket) => {
+    socket.on("send-location", (data) => {
+        io.emit("receive-location", { id: socket.id, ...data });
     });
     console.log("connected");
 
-    socket.on("disconnect",()=>{
-        io.emit("user-disconnect",socket.id)
-    })
-})
+    socket.on("disconnect", () => {
+        io.emit("user-disconnect", socket.id);
+    });
+});
 
-app.get('/',(req,res)=>{
-    res.render('index')
-})
+app.get('/', (req, res) => {
+    res.render('index');
+});
 
-server.listen(3000)
-
-module.exports = app;
+module.exports = {
+    app,
+    server
+};
